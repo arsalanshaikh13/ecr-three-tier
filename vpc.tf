5824042d2ed68c02cfa216b9bf8d84db21499ac6
arsalanshaikh13 <m.arsalanshaikh13@gmail.com>
2026-03-31 07:26:23 +0530

    added nat gateway and related route tables for private subnets and update db seeding job to wait for it to finish in deploy.yml

--- vpc.tf
+++ vpc.tf
@@ -99,3 +99,69 @@ resource "aws_subnet" "pri_sub_4b" {
     Name    = "pri-sub-4b"
   }
 }
+
+# Creating route table for vpc endpoint for vpc flow logs
+# create private route table Pri-RT-A and add route through NAT-GW-A
+resource "aws_route_table" "pri-rt-a" {
+  vpc_id            = aws_vpc.vpc.id
+
+  # route {
+  #   cidr_block      = "0.0.0.0/0"
+  #   # nat_gateway_id  = aws_nat_gateway.nat-a.id
+  #   network_interface_id   = aws_instance.nat_ec2_instance.primary_network_interface_id
+  # }
+
+  tags   = {
+    Name = "Pri-rt-a"
+  }
+}
+
+# NAT GATEWAY setup
+# Web tier
+# associate private subnet pri-sub-3-a with private route table Pri-RT-A
+resource "aws_route_table_association" "pri-sub-3a-with-Pri-rt-a" {
+  subnet_id         = aws_subnet.pri_sub_3a.id
+  route_table_id    = aws_route_table.pri-rt-a.id
+}
+
+# associate private subnet pri-sub-4b with private route table Pri-rt-b
+resource "aws_route_table_association" "pri-sub-4b-with-Pri-rt-b" {
+  subnet_id         = aws_subnet.pri_sub_4b.id
+  route_table_id    = aws_route_table.pri-rt-a.id
+}
+
+# allocate elastic ip. this eip will be used for the nat-gateway in the public subnet pub-sub-1-a
+resource "aws_eip" "eip-nat-a" {
+  # vpc    = true
+
+  tags   = {
+    Name = "eip-nat-a"
+  }
+}
+
+
+# create nat gateway in public subnet pub-sub-1a
+resource "aws_nat_gateway" "nat-a" {
+  allocation_id = aws_eip.eip-nat-a.id
+  subnet_id     = aws_subnet.pub_sub_1a.id
+
+  tags   = {
+    Name = "nat-a"
+  }
+
+  # to ensure proper ordering, it is recommended to add an explicit dependency
+  depends_on = [aws_internet_gateway.internet_gateway]
+}
+
+
+##########################################
+# Route Table Configuration
+##########################################
+
+# # Add route for private subnet traffic through NAT instance
+resource "aws_route" "nat_ec2_route" {
+    route_table_id         = aws_route_table.pri-rt-a.id
+    destination_cidr_block = "0.0.0.0/0"
+    nat_gateway_id   = aws_nat_gateway.nat-a.id
+}
+
